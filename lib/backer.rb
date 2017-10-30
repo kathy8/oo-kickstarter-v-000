58d3526386210da2cf1bea333ee675b133731857
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-10-30 21:04:17 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,7 @@ def back_project(project)
   @backed_projects
 end
 
-def back_project(projects)
+def back_project(project)
   backed_projects << project if project.is_a?(Project)
   project.backers << self if !project_backers.include?(self)
 end
