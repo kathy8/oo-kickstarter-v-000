92b64a2158337d5ba67d49d1a70c6ed356e9195c
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-10-30 19:59:58 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,6 @@
 Class Backer
 attr_accessor :name
+end 
 
 def initialize(name)
 @name = name
@@ -7,11 +8,11 @@ def initialize(name)
 end
 
 def back_project(project)
-  @backed_projects 
+  @backed_projects
 end
 
 def back_project(projects)
   backed_projects << project if project.is_a?(Project)
   project.backers << self if !project_backers.include?(self)
 end
-end 
+end
